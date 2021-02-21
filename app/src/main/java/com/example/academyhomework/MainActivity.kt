0c36f6055187a207f8d941245884c7bec487c2ba
Admin <itachi_172555@mail.ru>
2021-02-22 00:26:47 +0300

    com

--- app/src/main/java/com/example/academyhomework/MainActivity.kt
+++ app/src/main/java/com/example/academyhomework/MainActivity.kt
@@ -11,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.Toast
 import androidx.core.text.PrecomputedTextCompat
 import androidx.core.view.marginBottom
 import androidx.fragment.app.FragmentManager
@@ -34,30 +35,11 @@ class MainActivity : AppCompatActivity(), Throughoutable {
         setContentView(R.layout.activity_main)
         fragment = findViewById<androidx.fragment.app.FragmentContainerView>(R.id.main_container)
 
+
     }
 
 
-    fun onClickImage1(view: View){
-        fragment?.visibility = View.VISIBLE
-        findViewById<androidx.fragment.app.FragmentContainerView>(R.id.main_container)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, imageFragment.newInstance(R.drawable.ic_baseline_directions_bike_24))
-            .commit()
-    }
 
-    fun onClickImage2(view: View){
-        fragment?.visibility = View.VISIBLE
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, imageFragment.newInstance(R.drawable.ic_baseline_directions_run_24))
-            .commitNowAllowingStateLoss()
-    }
-
-    fun onClickImage3(view: View){
-        fragment?.visibility = View.VISIBLE
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, imageFragment.newInstance(R.drawable.ic_baseline_drive_eta_24))
-            .commit()
-    }
 
     override fun onClickToHide(viewButton: View) {
 
@@ -87,13 +69,22 @@ class MainActivity : AppCompatActivity(), Throughoutable {
     }
 
     fun onClickBottom(view: View) {
-        when(view.id)
+        fragment?.visibility = View.VISIBLE
+        val resImage = when(view.id)
         {
-            R.id.imageButton1 -> onClickImage1(view)
-            R.id.imageButton2 -> onClickImage2(view)
-            R.id.imageButton3 -> onClickImage3(view)
-
+            R.id.imageButton1 -> R.drawable.ic_baseline_directions_bike_24
+            R.id.imageButton2 -> R.drawable.ic_baseline_directions_run_24
+            R.id.imageButton3 -> R.drawable.ic_baseline_drive_eta_24
+            else -> R.drawable.shape_oval
         }
+        findViewById<Button>(R.id.imageButton1).setTextColor(resources.getColor(R.color.primaryTextColor, theme))
+        findViewById<Button>(R.id.imageButton2).setTextColor(resources.getColor(R.color.primaryTextColor, theme))
+        findViewById<Button>(R.id.imageButton3).setTextColor(resources.getColor(R.color.primaryTextColor, theme))
+        (view as Button).setTextColor(resources.getColor(R.color.secondaryColor, theme))
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, imageFragment.newInstance(resImage))
+            .commit()
 
 
     }
--- app/src/main/java/com/example/academyhomework/model/EnglishTranslate.kt
+++ app/src/main/java/com/example/academyhomework/model/EnglishTranslate.kt
@@ -1,7 +1,17 @@
 package com.example.academyhomework.model
 
+import com.example.academyhomework.data.DataSource
+
+
+
 data class EnglishTranslate(
     val engWord:String,
     val translateRus: String,
     val hint:String
-)
+){
+
+   enum class TranslateVariants(){
+        GOOD,
+       WRONG
+    }
+}
